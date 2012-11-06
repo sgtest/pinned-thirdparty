34af5d13b5bbbf7ab416e7685c5daa7ce0c7be4a
ms <ms@tpso.com>
2012-11-06 17:30:06 +0100

    .

--- commons-vfs2-2.0/build.sh
+++ commons-vfs2-2.0/build.sh
@@ -1,4 +1,9 @@
 cd core
 mvn    -Dmaven.test.skip=true install
+cd ..
 cd sandbox
 mvn    -Dmaven.test.skip=true install
+cd ..
+cp ./sandbox/target/commons-vfs2-sandbox-2.0.jar .
+cp ./core/target/commons-vfs2-2.0.jar .
+
